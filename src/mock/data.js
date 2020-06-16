4adab92eb4bc9e14cb8790aaa34a8a70c32b10ef
Tyler <tyler@geekingdoing.com>
2020-06-15 23:04:56 -0400

    Add about me text and project images

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,16 +11,19 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Tyler Deans',
-  subtitle: '',
+  subtitle: "I'm the Unknown Developer.",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'project.jpg',
-  paragraphOne: 'This is a test paragraph',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne:
+    'I am a software developer from Brooklyn, NY. I currently work for Infosys, a consulting company. I graduated with a Bachelor of Science degree in computer science.',
+  paragraphTwo:
+    'I have knowledge in the following programming languages: JavaScript, Python, and Java. I specialize in web development. The web frameworks I have used at work, side projects, or open source are Spring, React, Express, and Node.js. I primary use Oracle 11g as a SQL database.',
+  paragraphThree:
+    'I believe technology can be used to make a positive difference in the world. In my next role I would like to be a part of a company whose mission is to a positive impact.',
   resume: '', // if no resume, the button will not show up
 };
 
@@ -28,7 +31,7 @@ export const aboutData = {
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
+    img: 'myHealthLog.png',
     title: 'CoronaTracker',
     info:
       "An open source project I contribute to, it is an easy-to-use PWA to monitor the user's wellness and learn about COVID-19.",
@@ -38,7 +41,7 @@ export const projectsData = [
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
+    img: 'Expensify.png',
     title: 'Expensify',
     info: 'An app to add your expenses in order to understand where your money is going',
     info2: '',
@@ -47,7 +50,7 @@ export const projectsData = [
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
+    img: 'chatApp.png',
     title: 'Chat App',
     info: 'A basic chat application using Node.js, Express, and Socket.io',
     info2: '',
