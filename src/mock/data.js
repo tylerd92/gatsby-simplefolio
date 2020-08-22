d7b959d0651a01da963c3aadf4d8da12bd071ddf
Tyler Deans <twdeans@gmail.com>
2020-08-22 08:30:21 -0400

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Tyler Deans',
-  subtitle: "I'm a Front End Developer.",
+  subtitle: "I'm a Software Developer.",
   cta: '',
 };
 
